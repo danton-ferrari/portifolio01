3cc8458321577556a3a830da55a2384bb1f95eef
Danton Ferrari <dgabrielferrari7@gmail.com>
2023-12-19 18:05:13 -0300

    feat: final

--- src/app/components/experience/experience.tsx
+++ src/app/components/experience/experience.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image"
 import { SectionTitle } from "../sectionTitle/section-title"
 
 import "./experience.scss"
@@ -8,7 +9,76 @@ export function Experience(){
           <SectionTitle text="Experiencia"/>
           <p>oi</p>
           <div className="experience-time">
-
+            <div className="experience-language">
+              <Image
+              src="/react.png"
+                alt="React Logo"
+                width={40}
+                height={40}
+                priority
+              /> 
+              <div className="experience-unit">
+                <div className="experience-measure measure-1">
+                  3 meses
+                </div>
+              </div>
+            </div>
+            <div className="experience-language">
+              <Image
+              src="/js.png"
+                alt="Javascript Logo"
+                width={40}
+                height={40}
+                priority
+              /> 
+              <div className="experience-unit">
+                <div className="experience-measure measure-1">
+                  3 meses
+                </div>
+              </div>
+            </div>
+            <div className="experience-language">
+              <Image
+              src="/ts.png"
+                alt="Typescript Logo"
+                width={40}
+                height={40}
+                priority
+              />
+              <div className="experience-unit">
+                <div className="experience-measure measure-1">
+                  3 mese
+                </div>
+              </div> 
+            </div>
+            <div className="experience-language">
+              <Image
+              src="/java.png"
+                alt="Java Logo"
+                width={40}
+                height={40}
+                priority
+              /> 
+              <div className="experience-unit">
+                <div className="experience-measure measure-1">
+                  3 mese
+                </div>
+              </div>
+            </div>
+            <div className="experience-language">
+              <Image
+              src="/python.png"
+                alt="Python Logo"
+                width={40}
+                height={40}
+                priority
+              /> 
+              <div className="experience-unit">
+                <div className="experience-measure measure-2">
+                  <span>1 ano</span>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
     )
