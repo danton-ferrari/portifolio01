a098fd1196e4215a810366a1dde838083e91c528
DANTON GABRIEL NEIS FERRARI <dgabrielferrari7@gmail.com>
2024-01-03 22:17:33 -0300

    Remoção do "oi" da experiencia.

--- src/app/components/experience/experience.tsx
+++ src/app/components/experience/experience.tsx
@@ -1,85 +1,77 @@
-import Image from "next/image"
-import { SectionTitle } from "../sectionTitle/section-title"
+import Image from "next/image";
+import { SectionTitle } from "../sectionTitle/section-title";
 
-import "./experience.scss"
+import "./experience.scss";
 
-export function Experience(){
-    return(
-        <div className="experience">
-          <SectionTitle text="Experiencia"/>
-          <p>oi</p>
-          <div className="experience-time">
-            <div className="experience-language">
-              <Image
-              src="/react.png"
-                alt="React Logo"
-                width={40}
-                height={40}
-                priority
-              /> 
-              <div className="experience-unit">
-                <div className="experience-measure measure-1">
-                  3 meses
-                </div>
-              </div>
-            </div>
-            <div className="experience-language">
-              <Image
-              src="/js.png"
-                alt="Javascript Logo"
-                width={40}
-                height={40}
-                priority
-              /> 
-              <div className="experience-unit">
-                <div className="experience-measure measure-1">
-                  3 meses
-                </div>
-              </div>
-            </div>
-            <div className="experience-language">
-              <Image
-              src="/ts.png"
-                alt="Typescript Logo"
-                width={40}
-                height={40}
-                priority
-              />
-              <div className="experience-unit">
-                <div className="experience-measure measure-1">
-                  3 mese
-                </div>
-              </div> 
-            </div>
-            <div className="experience-language">
-              <Image
-              src="/java.png"
-                alt="Java Logo"
-                width={40}
-                height={40}
-                priority
-              /> 
-              <div className="experience-unit">
-                <div className="experience-measure measure-1">
-                  3 mese
-                </div>
-              </div>
-            </div>
-            <div className="experience-language">
-              <Image
-              src="/python.png"
-                alt="Python Logo"
-                width={40}
-                height={40}
-                priority
-              /> 
-              <div className="experience-unit">
-                <div className="experience-measure measure-2">
-                  <span>1 ano</span>
-                </div>
-              </div>
+export function Experience() {
+  return (
+    <div className="experience">
+      <SectionTitle text="Experiencia" />
+      <p></p>
+      <div className="experience-time">
+        <div className="experience-language">
+          <Image
+            src="/react.png"
+            alt="React Logo"
+            width={40}
+            height={40}
+            priority
+          />
+          <div className="experience-unit">
+            <div className="experience-measure measure-1">3 meses</div>
+          </div>
+        </div>
+        <div className="experience-language">
+          <Image
+            src="/js.png"
+            alt="Javascript Logo"
+            width={40}
+            height={40}
+            priority
+          />
+          <div className="experience-unit">
+            <div className="experience-measure measure-1">3 meses</div>
+          </div>
+        </div>
+        <div className="experience-language">
+          <Image
+            src="/ts.png"
+            alt="Typescript Logo"
+            width={40}
+            height={40}
+            priority
+          />
+          <div className="experience-unit">
+            <div className="experience-measure measure-1">3 mese</div>
+          </div>
+        </div>
+        <div className="experience-language">
+          <Image
+            src="/java.png"
+            alt="Java Logo"
+            width={40}
+            height={40}
+            priority
+          />
+          <div className="experience-unit">
+            <div className="experience-measure measure-1">3 mese</div>
+          </div>
+        </div>
+        <div className="experience-language">
+          <Image
+            src="/python.png"
+            alt="Python Logo"
+            width={40}
+            height={40}
+            priority
+          />
+          <div className="experience-unit">
+            <div className="experience-measure measure-2">
+              <span>1 ano</span>
             </div>
           </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
