5add97145f1a2c58798b37fffba0e4f6d84fec35
DANTON GABRIEL NEIS FERRARI <dgabrielferrari7@gmail.com>
2024-02-13 22:19:34 -0300

    Arrumando o link do Linkedin.

--- src/app/components/social-btns/social-btns.tsx
+++ src/app/components/social-btns/social-btns.tsx
@@ -3,23 +3,23 @@ import { InstaIcon } from "../icons/insta-icon";
 import { LinkedinIcon } from "../icons/linkedin-icon";
 import { TwitterIcon } from "../icons/twitter-icon";
 
-import "./social-btns.scss"
+import "./social-btns.scss";
 
-export function SocialBtns(){
-    return(
-        <div className="social">
-            <a href="https://www.instagram.com/danton.ferrari/">
-                <InstaIcon/>
-            </a>
-            <a href="https://br.linkedin.com/in/danton-gabriel-ferrari-25859621b">
-                <LinkedinIcon/>
-            </a>
-            <a href="https://github.com/danton-ferrari">
-                <GuithubIcon/>
-            </a>
-            <a href="https://twitter.com/g_danton7431">
-                <TwitterIcon/>
-            </a>
-        </div>
-    )
+export function SocialBtns() {
+  return (
+    <div className="social">
+      <a href="https://www.instagram.com/danton.ferrari/">
+        <InstaIcon />
+      </a>
+      <a href="https://br.linkedin.com/in/danton-ferrari">
+        <LinkedinIcon />
+      </a>
+      <a href="https://github.com/danton-ferrari">
+        <GuithubIcon />
+      </a>
+      <a href="https://twitter.com/g_danton7431">
+        <TwitterIcon />
+      </a>
+    </div>
+  );
 }
